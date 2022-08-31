482e3cfa74b1b8e7c17f1e1b3212b1a663fa5912
Oswaldo Luna <oswaldo.luna5345@alumnos.udg.mx>
2022-08-31 18:46:12 -0500

    first commit

---
+++ scripts.js
@@ -0,0 +1,14 @@
+
+function clearScreen() {
+    document.getElementById("result").value = "";
+}
+ 
+function mostrar(value) {
+    document.getElementById("result").value += value;
+}
+ 
+function calcular() {
+    var p = document.getElementById("result").value;
+    var q = eval(p);
+    document.getElementById("result").value = q;
+}
